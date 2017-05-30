9ec243ac535dd93d1301463e09c8a9e9d49f4f7e
Kobus Jonker <kobus.jonker@sgits.co.za>
2017-05-30 09:18:35 +0200

    Connecting to new API folder

--- src/shared/global-vars.ts
+++ src/shared/global-vars.ts
@@ -1,5 +1,5 @@
 //export const baseUrl = "http://10.0.0.187/JonkerBudget.WebApi/api";
-export const baseUrl = "http://www.crazyjonker.co.za/api";
+export const baseUrl = "http://www.crazyjonker.co.za/api/api";
 export const loginUrl = "http://sv-mobiledev1:8022/token";
 
 // export const googleMapsUrl = "http://maps.googleapis.com/maps/api/geocode/json?address=";
